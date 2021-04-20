10a53c6ab79e91e10095def6c2825f2242169095
wsomers <65971347+wsomers@users.noreply.github.com>
2021-04-20 14:54:50 -0400

    Update version.java

--- src/git_testing/version.java
+++ src/git_testing/version.java
@@ -2,6 +2,6 @@ package git_testing;
 
 class version {
 	public static void main(String[] args) {
-        System.out.println("Version: 1.0.0");
+        System.out.println("Version: 1.0.1");
 	}
 }
